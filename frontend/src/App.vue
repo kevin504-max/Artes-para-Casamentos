06549edcbaae01d8d306c2406ee824ce51c032cf
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-07-14 15:00:05 -0300

    feat: login/register component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -24,6 +24,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: #e9e9e9;
 }
 
 nav {
--- frontend/src/views/auth/RegisterPage.vue
+++ frontend/src/views/auth/RegisterPage.vue
@@ -1,23 +1,159 @@
 <template>
-    <div>
-        <h1>RegisterPage</h1>
+    <div class="container h-100 pt-lg-3">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-lg-12 col-xl-11">
+                <div class="card text-black" style="border-radius: 25px;">
+                    <div class="card-body p-md-5">
+                        <div class="row justify-content-center">
+                            <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                                
+                                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Cadastro</p>
+                                
+                                <form class="mx-1 mx-md-4" @submit.stop.prevent="register">
+                                    
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                            <input 
+                                            v-model="name"
+                                            type="text" id="form3Example1c" class="form-control" />
+                                            <label class="form-label mt-3" for="form3Example1c">Nome</label>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                            <input 
+                                            v-model="email"
+                                            type="email" id="form3Example3c" class="form-control" />
+                                            <label class="form-label mt-3" for="form3Example3c">E-mail</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-id-card-alt fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                            <input 
+                                            v-model="cpf"
+                                            type="text" id="form3Example3c" class="form-control" />
+                                            <label class="form-label mt-3" for="form3Example3c">CPF</label>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-phone fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                            <input 
+                                            v-model="cpf"
+                                            type="text" id="form3Example3c" class="form-control" />
+                                            <label class="form-label mt-3" for="form3Example3c">Phone</label>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                            <input 
+                                            v-model="password"
+                                            type="password" id="form3Example4c" class="form-control" />
+                                            <label class="form-label mt-3" for="form3Example4c">Senha</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="d-flex flex-row align-items-center mb-4">
+                                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                        <div class="form-outline flex-fill mb-0">
+                                            <input 
+                                            v-model="confirmed_password"
+                                            type="password" id="form3Example4c" class="form-control" />
+                                            <label class="form-label mt-3" for="form3Example4c">Confimar Senha</label>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                                        <button type="submit" class="btn primary-color btn-lg">Cadastrar</button>
+                                    </div>
+                                    
+                                </form>
+                                
+                            </div>
+                            <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                                
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                                class="img-fluid" alt="Sample image">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    name: "RegisterPage",
+    name: 'RegisterPage',
+    
     data () {
         return {
-            
-        }
+            'name': '',
+            'email': '',
+            'cpf': '',
+            'phone': '',
+            'password': '',
+            'confirmed_password': ''
+        };
     },
+    
     methods: {
-        
-    },
+        register () {
+            const payload = {
+                name: this.name,
+                email: this.email,
+                cpf: this.cpf,
+                phone: this.phone,
+                password: this.password,
+                confirmed_password: this.confirmed_password,
+            };
+            
+            axios.post('register', payload)
+            .then((response) => {
+                if (response.data.auth) {
+                    localStorage.setItem('access_token', response.data.access_token);
+                    this.$router.push({ name: 'home' });
+                    this.$swal({
+                        icon: 'success',
+                        title: 'Sucesso!',
+                        text: 'Cadastro realizado com sucesso!',
+                        showConfirmButton: false,
+                        timer: 2500
+                    })
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 2000);
+                }
+            }).catch((error) => {
+                console.log('Error: ', error.response.data);
+                this.$swal({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Algo deu errado! Tente novamente.',
+                    showConfirmButton: false,
+                    timer: 2500
+                })
+            });
+        }
+    }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.primary-color {
+    background-color: skyblue;
+    color: #222;
+}
 </style>
