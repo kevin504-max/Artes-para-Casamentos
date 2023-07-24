a44fc5607dd08ce85b611ba3375ec2b89fce7556
Kevin Lucas <66319272+kevin504-max@users.noreply.github.com>
2023-07-24 17:28:08 -0300

    refact: make carousel get fixed images

--- frontend/src/components/frontend/carousel/SliderComponent.vue
+++ frontend/src/components/frontend/carousel/SliderComponent.vue
@@ -13,9 +13,15 @@
             @sliding-end="onSlideEnd"
         >
             <b-carousel-slide
-                v-for="image in featuredImages"
-                :key="image"
-                :img-src="image"
+                :img-src="require('@/assets/carousel/1.jpg')"
+                alt="image"
+            ></b-carousel-slide>
+            <b-carousel-slide
+                :img-src="require('@/assets/carousel/2.jpg')"
+                alt="image"
+            ></b-carousel-slide>
+            <b-carousel-slide
+                :img-src="require('@/assets/carousel/3.png')"
                 alt="image"
             ></b-carousel-slide>
         </b-carousel>
@@ -23,36 +29,12 @@
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
     name: "SliderComponent",
-    inject: ['makeSpin'],
     data () {
         return {
             sliding: null,
-            featureds: [],
-            featuredImages: [],
         };
-    },
-    async mounted () {
-        this.makeSpin.value = true;
-
-        await axios.get('/home').then((response) => {
-            this.featureds = response.data.featureds;
-        }).catch((error) => {
-            console.log('Error Home Page: ', error)
-        })
-        
-        this.featureds.forEach((featured) => {
-            axios.get(`/${featured.id}/featuredImage`).then((response) => {
-                (response.data.status !== 200) ? this.featuredImages.push('@/assets/default-place.jpeg') : this.featuredImages.push(response.data.image_url);
-            }).catch((error) => {
-                console.log('Error Slider Image: ', error)
-            })
-        });
-
-        this.makeSpin.value = false;
     },
     methods: {
         onSlideStart () {
